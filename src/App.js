8241c2b6847b76518f5adae71d1283567e496658
lawrenceagles <lawrenceagles@gmail.com>
2021-04-04 17:49:48 +0100

    added theme.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,5 @@
-import { ThemeProvider } from 'styled-components';
-import theme from './theme';
-import Flex from './components/Flex';
-import Text from './components/Text';
-
 const App = () => {
-	return (
-		<ThemeProvider theme={theme}>
-			<Flex bg={'secondary'} p={6} mx={6} my={4}>
-				<Text textAlign="center">
-					This is a tomato box, with responsive width, some padding, and margin bottom
-				</Text>
-			</Flex>
-
-			{/* <Flex bg={'dirtyBrown'} p={6} mx={6} my={4}>
-				<Text textAlign="center">
-					This is a tomato box, with responsive width, some padding, and margin bottom
-				</Text>
-			</Flex> */}
-		</ThemeProvider>
-	);
+	return <div>Hello World!</div>;
 };
 
 export default App;
--- src/components/Text.js
+++
@@ -1,4 +0,0 @@
-import styled from 'styled-components';
-import { typography } from 'styled-system';
-const Text = styled.div`${typography};`;
-export default Text;
